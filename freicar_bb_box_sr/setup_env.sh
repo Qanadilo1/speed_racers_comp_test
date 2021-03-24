968b6c981242ba5b9187b021f8cd099f2f94030e
Seiryujin <davide.rezzoli@gmail.com>
2021-03-24 23:57:36 +0100

    Add files via upload

---
+++ freicar_bb_box_sr/setup_env.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+pip3 install numpy==1.16.4
+pip3 install bounding_box 
+pip3 install torch
+pip3 install pyyaml
+pip3 install webcolors
+pip3 install matplotlib
+pip3 install torchvision
+pip3 install webcolors
+pip3 install visdom
